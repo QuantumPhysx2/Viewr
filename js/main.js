9165247f9e9181cb748f2e4fbdf066c82053832d
QuantumPhysx2 <33999927+QuantumPhysx2@users.noreply.github.com>
2018-08-28 13:39:54 +0930

    Update 2.03

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// --- On Mouse Over ---
+// Mouse over script
+var elements = document.getElementsById("hidden");
+
+for (var i = 0; i < elements.length; i++) {
+  elements[i].onmouseover = mouseOver;
+  elements[i].onmouseout = mouseOut;
+}
+
+function mouseOver(element) {
+  element.style.color = "red";
+  document.getElementById("hidden").innerHTML = "Over";
+}
+
+function mouseOut(element) {
+  element.style.color = "blue";
+  document.getElementById("hidden").innerHTML = "Out";
+}
+
+// --- On Click Events ---
+// Drop down script
 function openDropMenu() {
   // Retrieve element by #id and override the 'show' .class properties
   document.getElementById("drop-category").classList.toggle("show");
@@ -13,7 +34,7 @@ window.onclick = function(evt) {
     for (i = 0; i < dropdowns.length; i++) {
       // Define variable which appends the iterable object until max amount
       var openDropDown = dropdowns[i];
-      
+
       if (openDropDown.classList.contains("show")) {
         openDropDown.classList.remove("show");
       }
