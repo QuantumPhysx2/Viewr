1616c79fd06887de40a0b73aa53ce093a9241349
QuantumPhysx2 <33999927+QuantumPhysx2@users.noreply.github.com>
2018-08-28 12:35:38 +0930

    Update 2.01

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 function openDropMenu() {
+  // Retrieve element by #id and override the 'show' .class properties
   document.getElementById("drop-category").classList.toggle("show");
 }
 
 window.onclick = function(evt) {
+  // If the click event is applied to the .class 'drop-btn'...
   if (!evt.target.matches(".drop-btn")) {
+    // Retrieve .class 'drop-content'
     var dropdowns = document.getElementsByClassName("drop-content");
     var i;
+    // ...For every iterable object in the .class...
     for (i = 0; i < dropdowns.length; i++) {
+      // Define variable which appends the iterable object until max amount
       var openDropDown = dropdowns[i];
-
+      
       if (openDropDown.classList.contains("show")) {
         openDropDown.classList.remove("show");
       }
