7afb2ac1cb9663bb63bdbdb7693718de35257705
QuantumPhysx2 <33999927+QuantumPhysx2@users.noreply.github.com>
2018-09-01 13:15:05 +0930

    Update 2.20

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // Mouse over script
 function mouseOver() {
+  // FIX this
+  // gimble == Retrieve svg 'title' property
   var gimble = this.title;
-  document.getElementById("name").innerHTML = "["+ gimble +"]" ;
+  // Change current element with title
+  document.getElementById("name").innerHTML = "["+ gimble +"]";
 }
 
 function mouseOut() {
@@ -11,7 +14,7 @@ function mouseOut() {
 // Drop down script
 document.addEventListener("click", function() {
   // Retrieve element by #id and override the 'show' .class properties
-  document.getElementById("drop-category").classList.toggle("show");
+  document.getElementById("drop-category").classList.toggle("show-menu");
 
   window.onclick = function(evt) {
     // If the click event is applied to the .class 'drop-btn'...
@@ -24,8 +27,8 @@ document.addEventListener("click", function() {
         // Define variable which appends the iterable object until max amount
         var openDropDown = dropdowns[i];
 
-        if (openDropDown.classList.contains("show")) {
-          openDropDown.classList.remove("show");
+        if (openDropDown.classList.contains("show-menu")) {
+          openDropDown.classList.remove("show-menu");
         }
       }
     }
