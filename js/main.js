a193933fc961094e03847447684d2c929da47c34
QuantumPhysx2 <33999927+QuantumPhysx2@users.noreply.github.com>
2018-09-01 10:29:36 +0930

    Update 2.18 -fix JS and Add Calendar

--- js/main.js
+++ js/main.js
@@ -9,25 +9,25 @@ function mouseOut() {
 }
 
 // Drop down script
-function openDropMenu() {
+document.addEventListener("click", function() {
   // Retrieve element by #id and override the 'show' .class properties
   document.getElementById("drop-category").classList.toggle("show");
-}
 
-window.onclick = function(evt) {
-  // If the click event is applied to the .class 'drop-btn'...
-  if (!evt.target.matches(".drop-btn")) {
-    // Retrieve .class 'drop-content'
-    var dropdowns = document.getElementsByClassName("drop-content");
-    var i;
-    // ...For every iterable object in the .class...
-    for (i = 0; i < dropdowns.length; i++) {
-      // Define variable which appends the iterable object until max amount
-      var openDropDown = dropdowns[i];
+  window.onclick = function(evt) {
+    // If the click event is applied to the .class 'drop-btn'...
+    if (!evt.target.matches(".drop-btn")) {
+      // Retrieve .class 'drop-content'
+      var dropdowns = document.getElementsByClassName("drop-content");
+      var i;
+      // ...For every iterable object in the .class...
+      for (i = 0; i < dropdowns.length; i++) {
+        // Define variable which appends the iterable object until max amount
+        var openDropDown = dropdowns[i];
 
-      if (openDropDown.classList.contains("show")) {
-        openDropDown.classList.remove("show");
+        if (openDropDown.classList.contains("show")) {
+          openDropDown.classList.remove("show");
+        }
       }
     }
   }
-}
+});
