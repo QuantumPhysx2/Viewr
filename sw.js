10d0d70b20a1ad7acb4754f28df9fd2590c8dfa8
Gia <33999927+QuantumPhysx2@users.noreply.github.com>
2018-10-13 15:15:18 +0930

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+var CACHE_TITLE = 'my-site-cache';
+var CACHE_VERSION = 'v1';
+var CACHE_NAME = CACHE_TITLE + '-' + CACHE_VERSION;
+var urlsToCache = [
+  "index.html",
+  "css/Style.css",
+  "js/main.js",
+  "js/mapControls.js",
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        console.log("Offline Fetch Success");
+        return response;
+      }
+      return fetch(event.request);
+    }
+  )
+);
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(caches.keys().then(function(cacheNames) {
+      return Promise.all(cacheNames.map(function(cacheName) {
+          if(cacheName !== CACHE_NAME && cacheName.indexOf(CACHE_TITLE) === 0) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
